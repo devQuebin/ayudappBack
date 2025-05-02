6e13666cf3c15077b39b8bed07946375d2586f17
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2025-05-01 23:33:11 -0300

    Fix import path for Firebase configuration and correct user creation response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { collection, doc, getDoc, addDoc, updateDoc, deleteDoc, getDocs, serverTimestamp } from 'firebase/firestore';
 import { addCreatedTimestamps } from '../utils/firestore_utils.js';
-import db from '../../firebase_config.js';
+import db from '../config/firebase_config.js';
 
 
 // Create a new user
@@ -8,8 +8,8 @@ export const createUser = async (req, res) => {
     try {
         const body = req.body;
         const userRef = collection(db, 'user').withConverter(addCreatedTimestamps);
-        await addDoc(userRef, body);
-        res.status(201).json({ message: "User created successfully", id: userRef.id });
+        const docRef = await addDoc(userRef, body); 
+        res.status(201).json({ message: "User created successfully", id: docRef.id });
     } catch (error) {
         console.error("Error creating user:", error);
         res.status(500).json({ message: "Error creating user", error: error.message });
