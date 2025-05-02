562f0189f8141ce545b7cc4827d7c0b13bf6d3d4
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2025-05-02 13:17:58 -0300

    Refactor code style for consistency in firebase_config.js and firestore_utils.js

--- src/utils/firestore_utils.js
+++ src/utils/firestore_utils.js
@@ -1,29 +1,28 @@
-import { serverTimestamp } from 'firebase/firestore';
+import { serverTimestamp } from "firebase/firestore";
 
 function formatMesAnio(timestamp) {
-    if (!timestamp || !timestamp.toDate) return null;
-    const date = timestamp.toDate();
-    const mes = date.toLocaleString('es-ES', { month: 'long' });
-    const año = date.getFullYear();
-    return `${mes} ${año}`;
+  if (!timestamp || !timestamp.toDate) return null;
+  const date = timestamp.toDate();
+  const mes = date.toLocaleString("es-ES", { month: "long" });
+  const año = date.getFullYear();
+  return `${mes} ${año}`;
 }
 
 export const addCreatedTimestamps = {
-    toFirestore: (docData) => {
-        return {
-            created_at: serverTimestamp(),
-            updated_at: serverTimestamp(),
-            ...docData,
-        }
-    },
-    fromFirestore: (snapshot, options) => {
-        const data = snapshot.data(options)
-        return {
-            id: snapshot.id,
-            ...data,
-            created_at: formatMesAnio(data.created_at),
-            updated_at: formatMesAnio(data.updated_at),
-        }
-    },
-}
-
+  toFirestore: (docData) => {
+    return {
+      created_at: serverTimestamp(),
+      updated_at: serverTimestamp(),
+      ...docData,
+    };
+  },
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      ...data,
+      created_at: formatMesAnio(data.created_at),
+      updated_at: formatMesAnio(data.updated_at),
+    };
+  },
+};
