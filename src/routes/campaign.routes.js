caf71b2cfa03d747b5301ac0defe7b0c0f350e89
Lucascn21 <34776035+Lucascn21@users.noreply.github.com>
2025-05-02 15:35:21 -0300

    Add validation middleware to campaign routes for improved request handling

--- src/routes/campaign.routes.js
+++ src/routes/campaign.routes.js
@@ -6,17 +6,35 @@ import {
   updateCampaign,
   deleteCampaign,
 } from "../controllers/campaign.controller.js";
+import {
+  validateCreateCampaign,
+  validateParamCampaignId,
+  validateUpdateCampaign,
+} from "../validators/campaign.validator.js";
+import { uniqueField } from "../middlewares/uniqueField.middleware.js";
+import { fieldDoesntExist } from "../middlewares/fieldDoesntExist.middleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllCampaigns);
 
-router.get("/:id", getCampaignById);
+router.get("/:id", validateParamCampaignId, getCampaignById);
 
-router.post("/", createCampaign);
+router.post(
+  "/",
+  validateCreateCampaign,
+  uniqueField("campaign", "name"),
+  createCampaign
+);
 
-router.put("/:id", updateCampaign);
+router.put(
+  "/:id",
+  validateParamCampaignId,
+  fieldDoesntExist("campaign", "name"),
+  validateUpdateCampaign,
+  updateCampaign
+);
 
-router.delete("/:id", deleteCampaign);
+router.delete("/:id", validateParamCampaignId, deleteCampaign);
 
 export default router;
