5e436000cad4567c6c6a822bbffe928816509f61
MatiasAFare <matiasfare@gmail.com>
2025-05-12 17:22:14 -0300

    Fixed login endpoint

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,12 +3,12 @@ import express from "express";
 const router = express.Router();
 
 import {
-    registerUser, 
-    loginUser, 
+    registerUser,
+    loginUser,
 } from "../controllers/auth.controller.js";
 
 
 router.post("/register", registerUser);
-router.post("login", loginUser);
+router.post("/login", loginUser);
 
 export default router;
