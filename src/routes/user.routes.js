05ba2f44123a1566db42bf4d9641262b06249e1e
lazaronazareno <lazaronazareno@gmail.com>
2025-06-23 19:03:03 -0300

    Refactor user controller to integrate Firebase Admin SDK for user management and update routes and validators accordingly

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"
 import {
   getAllUsers,
   getUserById,
@@ -6,62 +6,63 @@ import {
   updateUser,
   deleteUser,
   getUsersByCampaign,
-  loginUser,
-} from "../controllers/user.controller.js";
+  getUserProfile,
+} from "../controllers/user.controller.js"
 
 import {
   validateCreateUser,
   validateUpdateUser,
-  validateLoginUser,
-} from "../validators/user.validator.js";
+} from "../validators/user.validator.js"
 
 import {
   paramIdSchema,
   validateParams,
-} from "../validators/common.validator.js";
+} from "../validators/common.validator.js"
 
-import { uniqueField } from "../middlewares/uniqueField.middleware.js";
-import { fieldDoesntExist } from "../middlewares/fieldDoesntExist.middleware.js";
-import { isAuthenticated } from "../middlewares/auth.middleware.js";
+import { uniqueField } from "../middlewares/uniqueField.middleware.js"
+import { fieldDoesntExist } from "../middlewares/fieldDoesntExist.middleware.js"
+import { isAuthenticated } from "../middlewares/auth.middleware.js"
 
-const router = express.Router();
+const router = express.Router()
 
 // Get all users
-router.get("/", getAllUsers);
+router.get("/", getAllUsers)
 // Get user by ID
-router.get("/:userId", validateParams(paramIdSchema("userId")), getUserById);
+router.get("/:userId", validateParams(paramIdSchema("userId")), getUserById)
 // Create a new user
 router.post(
   "/",
   isAuthenticated,
   validateCreateUser,
-  uniqueField("user", "email"),
+  uniqueField("users", "email"),
   createUser
-);
+)
 
-router.post("/login", validateLoginUser, loginUser);
+// Get user profile (authenticated user)
+router.get("/profile", isAuthenticated, getUserProfile)
+
 // Update user by ID
 router.put(
   "/:userId",
   isAuthenticated,
   validateParams(paramIdSchema("userId")),
-  fieldDoesntExist("user", "email"),
+  fieldDoesntExist("users", "email"),
   validateUpdateUser,
   updateUser
-);
+)
 // Delete user by ID
 router.delete(
   "/:userId",
   isAuthenticated,
   validateParams(paramIdSchema("userId")),
   deleteUser
-);
+)
 // Get users by campaign ID
 router.get(
   "/campaign/:campaignId",
   isAuthenticated,
   validateParams(paramIdSchema("campaignId")),
   getUsersByCampaign
-);
+)
 
-export default router;
+export default router
